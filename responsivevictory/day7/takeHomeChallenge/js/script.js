b9639f9a291e7b8839fcb8d0ffbe4105e716c62f
kris kuchinka <kriskuchinka@gmail.com>
2023-04-27 20:09:57 -0700

    started take home challenge.

---
+++ responsivevictory/day7/takeHomeChallenge/js/script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to the fortune teller...");
+
+var numChildren = 0;
+var partner = "Brielle";
+var location = "Vancouver";
+var jobTitle = "Peer Support/Developer in Training";
+var birthYear = 1988;
+var futureYear = 2038;
+
+console.log(
+  `You will be a ${jobTitle} in ${location}, and married to ${partner} with ${numChildren} kids.`
+);
+
