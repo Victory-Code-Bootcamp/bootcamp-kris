6c7563d8353bfe4eaef2ec0e1740535fd951d38a
kris kuchinka <kriskuchinka@gmail.com>
2023-04-27 19:09:26 -0700

    first javascript attempts.

---
+++ responsivevictory/day7/dailyChallenge/js/script.js
@@ -0,0 +1,16 @@
+console.log("welcome to the console.");
+
+var myFirstName = "Kris";
+console.log(myFirstName);
+console.log(myFirstName.length);
+var luckyNumberOne = 13;
+var luckyNumberTwo = 7;
+console.log(
+  "My first lucky number is " +
+    luckyNumberOne +
+    ". My second lucky number is " +
+    luckyNumberTwo +
+    "."
+);
+var undefinedVariable;
+console.log(undefinedVariable);
