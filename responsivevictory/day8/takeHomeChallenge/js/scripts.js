28066e1c8141625e925b4df26b4c2a7e3ec1587e
kris kuchinka <kriskuchinka@gmail.com>
2023-05-03 21:06:29 -0700

    finished math challenge #2

--- responsivevictory/day8/takeHomeChallenge/js/scripts.js
+++ responsivevictory/day8/takeHomeChallenge/js/scripts.js
@@ -1,10 +1,20 @@
 console.log(`Hello, World!`);
 
 // Math Challenges
+// Math Challenge #1
+// Store two numbers in two different variables. Create another variable that determines the biggest number of the two
 const NUM_1 = prompt(`Enter a number. Any number.`);
 const NUM_2 = prompt(`Enter another number. Any number`);
 let bigNum = Math.max(NUM_1, NUM_2);
 console.log(`The bigger number between ${NUM_1} and ${NUM_2} is ${bigNum}`);
 alert(
   `You entered ${NUM_1} and ${NUM_2}. Of the two numbers, the bigger is ${bigNum}`
+);
+
+// Math Challenge #2
+// Create a variable that generates a random number that is rounded down to the nearest integer
+
+let randomNumber = Math.floor(Math.random() * 100);
+console.log(
+  `Your randomly generated number between 1 and 100 is ${randomNumber}`
 );
