970d4cdf542e42354a0a29c27f0f9cdf19f8b2e0
kris kuchinka <kriskuchinka@gmail.com>
2023-05-09 18:46:30 -0700

    first challenge completed

---
+++ responsivevictory/day10/js/scripts.js
@@ -0,0 +1,17 @@
+console.log("Hello, World!");
+// Global vs. Local Scope Challenge
+// Write a function that declares a variable inside the function and another variable outside the function. Call the function and try to access both variables inside and outside the function. Observe the behavior and explain what happened.
+
+let harborLocal = () => {
+  let insideVar = "This is a local variable.";
+  console.log(insideVar);
+};
+// Set up global variable
+let outsideVar = "This is a global variable.";
+console.log(outsideVar);
+// Call the function
+harborLocal();
+// Attempt to use local variable outside of function just to watch it fail
+console.log(insideVar);
+
+// Odd or Even Challenge
