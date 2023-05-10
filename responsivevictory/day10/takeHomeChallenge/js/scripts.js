ee3bb2ed4efc699b12239ba851b148fcc402124a
kris kuchinka <kriskuchinka@gmail.com>
2023-05-09 20:23:58 -0700

    file structure and comments loaded.

---
+++ responsivevictory/day10/takeHomeChallenge/js/scripts.js
@@ -0,0 +1,7 @@
+console.log("Let's play with Pokemon.");
+// Create an async function that:
+// Makes a request to the Pokemon API using fetch that passes in a pokemon that the user requests
+// Makes a request to the Pokemon API using fetch that passes in a pokemon that the user requests
+// Makes sure we use .json()
+// Display the name of the Pokemon that you chose in an alert
+
