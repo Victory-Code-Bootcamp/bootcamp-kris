9e0042f76faaf5cd9ac94b07c3ed794bd91ff595
kris kuchinka <kriskuchinka@gmail.com>
2023-05-10 20:15:03 -0700

    made the API work. time to attempt to get fancy.

--- responsivevictory/day10/takeHomeChallenge/js/scripts.js
+++ responsivevictory/day10/takeHomeChallenge/js/scripts.js
@@ -1,7 +1,21 @@
 console.log("Let's play with Pokemon.");
 // Create an async function that:
 // Makes a request to the Pokemon API using fetch that passes in a pokemon that the user requests
-// Makes a request to the Pokemon API using fetch that passes in a pokemon that the user requests
+// Stores this request into a variable
 // Makes sure we use .json()
 // Display the name of the Pokemon that you chose in an alert
+window.onload = function () {
+  document.getElementById("pokeButton").addEventListener("click", function () {
+    let pokeChoice = document.getElementById("pokeInput").value;
+    let pokeSelection = `Your chosen Pokemon is: ${pokeChoice}.`;
+    console.log(pokeSelection);
+  });
+};
+// Hard-Coded Version
+// async function pokeGrab() {
+//   const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+//   const pokemon = await response.json();
+//   console.log(pokemon);
+// }
 
+// pokeGrab();
