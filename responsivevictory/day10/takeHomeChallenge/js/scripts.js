c03b88894d349da73869a544c9865bc693184403
kris kuchinka <kriskuchinka@gmail.com>
2023-05-10 21:06:23 -0700

    exciting additions with APIs

--- responsivevictory/day10/takeHomeChallenge/js/scripts.js
+++ responsivevictory/day10/takeHomeChallenge/js/scripts.js
@@ -6,11 +6,46 @@ console.log("Let's play with Pokemon.");
 // Display the name of the Pokemon that you chose in an alert
 window.onload = function () {
   document.getElementById("pokeButton").addEventListener("click", function () {
-    let pokeChoice = document.getElementById("pokeInput").value;
+    let pokeChoice = document.getElementById("pokeInput").value.toLowerCase();
     let pokeSelection = `Your chosen Pokemon is: ${pokeChoice}.`;
     console.log(pokeSelection);
+    selectPokemon(pokeChoice);
   });
 };
+
+async function selectPokemon(pokeSelection) {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokeSelection}`
+  );
+  const chosenPoke = await response.json();
+  console.log(chosenPoke);
+
+  // Create a new div element to hold the Pokemon data
+  const pokeDataDiv = document.createElement("div");
+
+  // Add the pokemon name to the div
+  const pokeNameElement = document.createElement("p");
+  pokeNameElement.textContent = `Name: ${chosenPoke.name}`;
+  pokeDataDiv.appendChild(pokeNameElement);
+
+  // Add the pokemon sprites to the div as a list  
+  const pokeSpritesElement = document.createElement("p");
+  pokeSpritesElement.textContent = `Sprites: `;
+  // Add the pokemon abilities to the div as a list
+  const pokeAbilitiesElement = document.createElement("p");
+  pokeAbilitiesElement.textContent = `Abilities: `;
+  const pokeAbilitiesList = document.createElement("ul");
+  for (let i = 0; i < chosenPoke.abilities.length; i++) {
+    const pokeAbilityListItem = document.createElement("li");
+    pokeAbilityListItem.textContent = chosenPoke.abilities[i].ability.name;
+    pokeAbilitiesList.appendChild(pokeAbilityListItem);
+  }
+  pokeAbilitiesElement.appendChild(pokeAbilitiesList);
+  pokeDataDiv.appendChild(pokeAbilitiesElement);
+
+  // Add the div to the HTML page
+  document.getElementById("pokeData").appendChild(pokeDataDiv);
+}
 // Hard-Coded Version
 // async function pokeGrab() {
 //   const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
