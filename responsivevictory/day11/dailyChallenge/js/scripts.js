97e85be938db6fe7e1731ccdac6d29da918d00b9
kris kuchinka <kriskuchinka@gmail.com>
2023-05-11 21:25:37 -0700

    Challenge 2 completed

--- responsivevictory/day11/dailyChallenge/js/scripts.js
+++ responsivevictory/day11/dailyChallenge/js/scripts.js
@@ -22,3 +22,18 @@ let fizzBuzz = () => {
 };
 
 fizzBuzz();
+
+// Write a function that takes in an array of numbers and returns the sum of all the even numbers in the array.
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let evenSum = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+};
+
+console.log(evenSum(array));
