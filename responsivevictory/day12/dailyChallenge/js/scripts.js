eb7cfbf34cf8757b88121922f7c77932e01230cd
kris kuchinka <kriskuchinka@gmail.com>
2023-05-17 20:39:38 -0700

    challenge 1 accomplished.

--- responsivevictory/day12/dailyChallenge/js/scripts.js
+++ responsivevictory/day12/dailyChallenge/js/scripts.js
@@ -1,1 +1,15 @@
 console.log("Welcome to the Daily Challenge, Day 12!");
+
+// Challenge 1
+// Create a new object called Donis Kia
+// Donis Kia should store key value pairs of things the car could have
+// One of the keys stored should be a playlist that contains a list of Donis favorite songs
+
+const donisKia = {
+  keys: true,
+  year: 2024,
+  color: "teal",
+  playlist: ["Mathematics", "Get By", "4 Winds"],
+};
+
+
