eebb1b0d454c744cd50e06c9331da305561ede9f
kris kuchinka <kriskuchinka@gmail.com>
2023-05-21 12:50:25 -0700

    need verification: is this challenge correct (challenge 2)?

--- js/day1/dailyChallenge/js/scripts.js
+++ js/day1/dailyChallenge/js/scripts.js
@@ -11,6 +11,20 @@ console.log(combinedArrays);
 // Challenge 2: Object Spread
 // Create a new object by merging multiple objects using the spread operator.
 
+function Person(name, age, eyeColor) {
+  this.name = name;
+  this.age = age;
+  this.eyeColor = eyeColor;
+}
+
+const person1 = new Person("Kris", 35, "Hazel");
+const person2 = new Person("Brielle", 25, "Blue");
+const person3 = new Person("Clara", 3, "Brown");
+
+const combinedPersons = { ...person1, ...person2, ...person3 };
+
+console.log(combinedPersons);
+
 // Challenge 3: Array Rest
 // Create a function that accepts a variable number of arguments and uses the rest operator to gather them into an array.
 
